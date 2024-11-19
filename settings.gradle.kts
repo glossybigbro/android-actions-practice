6d19eb7af090b1e3983f591513d848f3aa543d78
hellomarket-isaac <isaac@hellomarket.com>
2024-11-19 17:20:24 +0900

    add build scan workflow and CI environment settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,12 +10,39 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.gradle.develocity") version("3.18.2")
+    }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.develocity")
+}
+
+val isCI = !System.getenv("CI").isNullOrEmpty()
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground.set(!isCI)
+        capture.fileFingerprints.set(true)
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = !isCI
+        removeUnusedEntriesAfterDays = 30
     }
 }
 
